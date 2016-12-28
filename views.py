240eb97e6c3cc15f62481d8df3791feaaec018b2
lnlantian <995905915@qq.com>
2016-12-28 09:18:33 +0800

    Update views.py

--- views.py
+++ views.py
@@ -1,4 +1,13 @@
 # create the view for blog show
+#coding=utf-8
+from django.shortcuts import render
+from blog.models import BlogsPost
+from django.shortcuts import render_to_response
+
+# Create your views here.
+def index(request):
+    blog_list = BlogsPost.objects.all()
+    return render_to_response('index.html',{'blog_list':blog_list})
 
 def myBlogs(request):
     blog_list = BlogPost.objects.all()
