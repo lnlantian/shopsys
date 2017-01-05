4498b925b1813d008e3450c3fc8a5f1157e7927a
Lantianor <lnlantian@users.noreply.github.com>
2017-01-05 09:23:24 +0800

    Update views.py

--- views.py
+++ views.py
@@ -1,14 +1,30 @@
-# create the view for blog show
-#coding=utf-8
-from django.shortcuts import render
-from blog.models import BlogsPost
-from django.shortcuts import render_to_response
-
-# Create your views here.
-def index(request):
-    blog_list = BlogsPost.objects.all()
-    return render_to_response('index.html',{'blog_list':blog_list})
-
-def myBlogs(request):
-    blog_list = BlogPost.objects.all()
-    return render_to_response('BlogTemplate.html',{'blog_list':blog_list})
+from django.shortcuts import render  
+import logging  
+from django.conf import settings  
+from .models import *  
+from django.core.paginator import Paginator, InvalidPage, EmptyPage, PageNotAnInteger ##导入  
+  
+# Get an instance of a logger  
+logger = logging.getLogger(__name__)  
+  
+def global_setting(request):  
+    return {'SITE_NAME': settings.SITE_NAME,  
+            'WEIBO_SINA': settings.WEIBO_SINA}  
+  
+  
+def index(request):  
+    try:  
+        categories = Cateory.objects.all()  
+        articles = Article.objects.all()  
+        paginator = Paginator(articles, 2) #分页  
+        try:  
+            page = int(request.GET.get('page', 1))  
+            articles = paginator.page(page)  
+        except PageNotAnInteger:  
+            articles = paginator.page(1)  
+        except EmptyPage:  
+            articles = paginator.page(paginator.num_pages)  
+  
+    except Exception as e:  
+        logger.error(e)  
+    return render(request, 'index.html', locals())  
