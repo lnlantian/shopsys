2e7618d939b3730f016b5d31eb4f314a5a48bc88
Lantianor <lantianor@126.com>
2017-01-08 11:53:07 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from MyBlog.views import *
 
-
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^myBlogs/$',myBlogs),
+    url(r'^Blogs/$',Blogs),
 ]
